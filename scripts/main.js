a4be3437ed5eeed77f2ac3796be596105e0ef4c5
Peter Velkov <kidroca@gmail.com>
2017-09-03 04:02:28 +0300

    refactor(scripts): add `not ready` script

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    setTimeout(function() {
+        document.querySelector('.under-construction').classList.add('slideDown');
+    }, 1500);
+});
